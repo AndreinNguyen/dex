5200096d941a3e5dbbe3a3aa22003802cee40b25
Tuanpep <tuanbt209@gmail.com>
2022-07-01 09:53:45 +0700

    change router address

--- src/config/constants/exchange.ts
+++ src/config/constants/exchange.ts
@@ -5,7 +5,7 @@ import { ChainTokenList } from './types'
 
 export const ROUTER_ADDRESS = {
   [ChainId.MAINNET]: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
-  [ChainId.TESTNET]: '0xD99D1c33F9fC3444f8101754aBC46c52416550D1',
+  [ChainId.TESTNET]: '0x0863334437086090A7cD6c5Fcc4D3B208bab2bef',
 }
 
 // used to construct intermediary pairs for trading
