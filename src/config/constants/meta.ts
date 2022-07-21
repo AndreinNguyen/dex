937884a509e156d89dc36669fb5cd99f77b2200a
AndreinChef <nguyen.nguyen@savvycomsoftware.com>
2022-07-21 16:56:03 +0700

    Chore: Tittle description

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -5,7 +5,7 @@ import { PageMeta } from './types'
 export const DEFAULT_META: PageMeta = {
   title: 'SavvyDex',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
+    'The most popular AMM on BSC by user count! Earn SVC through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by SavvyDex), NFTs, and more, on a platform you can trust.',
   image: '/images/Savvycoin-logo.png',
 }
 
