e1b55d83b8fb905906936c875eb3eb17d7e682f9
Tuanpep <tuanbt209@gmail.com>
2022-07-21 15:44:49 +0700

    fix build

--- src/pages/pools/index.tsx
+++ src/pages/pools/index.tsx
@@ -1,8 +1,5 @@
 import { useWeb3React } from '@web3-react/core'
-import { BubbleHelper } from 'components/BubbleHelper'
-import { POOL_DOCS_URL } from 'config/constants'
 import { useContext } from 'react'
-import { usePriceCakeBusd } from 'state/farms/hooks'
 import { FarmsContext, FarmsPageLayout } from 'views/FarmSvc'
 import FarmCard from 'views/FarmSvc/components/FarmCard/FarmCard'
 import { getDisplayApr } from 'views/FarmSvc/Farms'
@@ -10,8 +7,6 @@ import { getDisplayApr } from 'views/FarmSvc/Farms'
 const FarmsPage = () => {
   const { account } = useWeb3React()
   const { chosenFarmsMemoized } = useContext(FarmsContext)
-
-  const cakePrice = usePriceCakeBusd()
 
   return (
     <>
