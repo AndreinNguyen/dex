da995ba6c5797e2684f080363a6a4d30f508f2b8
AndreinChef <nguyen.nguyen@savvycomsoftware.com>
2022-08-04 14:54:03 +0700

    Feat: Add unicorn avatar for need help

--- src/components/BubbleHelper/BubbleHelper.tsx
+++ src/components/BubbleHelper/BubbleHelper.tsx
@@ -1,13 +1,24 @@
 import { Button, Flex, Svg } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import styled from 'styled-components'
+import Image from 'next/image'
+import lotteryImage from '../../../public/images/home/unicorn.png'
 
-const BubbleWrapperContainer = styled.div`
+const Container = styled.div`
+  position: absolute;
   display: flex;
   bottom: 0;
   right: 0;
-  position: absolute;
-  transform: translateY(50px);
+  z-index: 100;
+  transform: translate(130px, 200px);
+  display: none;
+  ${({ theme }) => theme.mediaQueries.md} {
+    display: flex;
+  }
+`
+
+const BubbleWrapperContainer = styled.div`
+  transform: translate(35px, 40%);
 
   ${({ theme }) => theme.mediaQueries.sm} {
     bottom: 3%;
@@ -31,6 +42,11 @@ const BubbleWrapper = styled(Flex)`
   }
 `
 
+const ImageWrapper = styled(Image)`
+  -webkit-transform: scaleX(-1) !important;
+  transform: scaleX(-1) !important;
+`
+
 type Props = {
   helpUrl: string
 }
@@ -39,16 +55,19 @@ const BubbleHelper = ({ helpUrl }: Props) => {
   const { t } = useTranslation()
 
   return (
-    <BubbleWrapperContainer>
-      <BubbleWrapper>
-        <Button id="clickExchangeHelp" as="a" external href={helpUrl} variant="subtle">
-          {t('Need help ?')}
-        </Button>
-        <Svg viewBox="0 0 16 16">
-          <path d="M0 16V0C0 0 3 1 6 1C9 1 16 -2 16 3.5C16 10.5 7.5 16 0 16Z" />
-        </Svg>
-      </BubbleWrapper>
-    </BubbleWrapperContainer>
+    <Container>
+      <BubbleWrapperContainer>
+        <BubbleWrapper>
+          <Button id="clickExchangeHelp" as="a" external href={helpUrl} variant="subtle">
+            {t('Need help ?')}
+          </Button>
+          <Svg viewBox="0 0 16 16">
+            <path d="M0 16V0C0 0 3 1 6 1C9 1 16 -2 16 3.5C16 10.5 7.5 16 0 16Z" />
+          </Svg>
+        </BubbleWrapper>
+      </BubbleWrapperContainer>
+      <ImageWrapper src={lotteryImage} width={200} height={340} alt="LotteryBanner" placeholder="blur" />
+    </Container>
   )
 }
 
